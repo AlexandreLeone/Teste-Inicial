58112b8cec8699d12816841738a15713200766d7
Alexandre Leone <alexandreleone@gmail.com>
2013-11-04 04:05:14 -0200

    Primeira

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrimeiroTeste::Application.config.secret_token = '4500d7f5006ee4c11edc3cf5565770b0dab0a0084e6c0496400a69db937bb49f5259e5990b4ef6c12627b7d562a9b7325fb88f0bf7ecdaa93b811a9e106d33f0'
